36d532110d4ce8ba722c66adb004d4f96cc851e1
Mihaela <strechii.mihaela@gmail.com>
2023-06-19 16:00:13 +0200

    initial commit

---
+++ src/components/teams/TeamMembers.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <h2>{{ teamName }}</h2>
+    <ul>
+      <user-item
+        v-for="member in members"
+        :key="member.id"
+        :name="member.fullName"
+        :role="member.role"
+      ></user-item>
+    </ul>
+  </section>
+</template>
+
+<script>
+import UserItem from '../users/UserItem.vue';
+
+export default {
+  components: {
+    UserItem
+  },
+  data() {
+    return {
+      teamName: '',
+      members: []
+     
+    };
+  },
+
+  inject: ['teams', 'users'],
+
+  created() {
+    const teamId = this.$route.params.teamId;
+    const team = this.teams.find((team) => team.id === teamId);
+    this.teamName = team.name;
+    this.members = this.users.filter((user) => team.members.includes(user.id));
+  },
+  }
+</script>
+
+<style scoped>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+h2 {
+  margin: 0.5rem 0;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
